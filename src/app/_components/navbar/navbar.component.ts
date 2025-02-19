6e38f0cbbebb49be6340307ac35fdaae72bd49db
Kauã <kauaherrmann@hotmail.com>
2025-02-18 22:12:30 -0300

    Adição do cabeçalho

--- src/app/_components/navbar/navbar.component.ts
+++ src/app/_components/navbar/navbar.component.ts
@@ -7,5 +7,10 @@ import { Component, HostListener } from '@angular/core';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  isScrolled = false;
 
+  @HostListener('window:scroll', [])
+  onScroll() {
+    this.isScrolled = window.scrollY > 50; // Quando rolar mais de 50px, troca a navbar
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
@@ -15,7 +14,8 @@ import { NavbarComponent } from './_components/navbar/navbar.component';
   imports: [
     BrowserModule,
     AppRoutingModule
-  ],
+
+],
   providers: [],
   bootstrap: [AppComponent]
 })
