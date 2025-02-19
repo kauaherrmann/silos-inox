5ea6fd2f15d8bd8965d2dd3b71d4518d8d206bfe
Kauã <kauaherrmann@hotmail.com>
2025-02-18 21:08:41 -0300

    Silos-inox.v2

---
+++ src/app/_components/navbar/navbar.component.ts
@@ -0,0 +1,11 @@
+import { Component, HostListener } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: false,
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+export class NavbarComponent {
+
+}
